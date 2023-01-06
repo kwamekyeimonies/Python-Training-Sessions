36097eb3ea43bffe0b4a889798577471d53bf547
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-01-06 11:24:52 +0000

    python refresher end

--- ooad_dev.py
+++ ooad_dev.py
@@ -137,23 +137,39 @@
 # print(joysticks)
             
             
-class BookShelf:
-    def __init__(self,*books):
-        self.books = books
+# class BookShelf:
+#     def __init__(self,*books):
+#         self.books = books
     
-    def __str__(self):
-        return f"Bookshelf with {len(self.books)} "
+#     def __str__(self):
+#         return f"Bookshelf with {len(self.books)} "
     
 
-class Book():
-    def __init__(self,name):
-        self.name = name
+# class Book():
+#     def __init__(self,name):
+#         self.name = name
         
-    def __str__(self):
-        return f"Book {self.name} "
+#     def __str__(self):
+#         return f"Book {self.name} "
         
-book= Book("King Solomon Mines")
-book1= Book("Golang Microservices 101")
-book3= Book("The god's of Egypt")
-shelf = BookShelf(book,book1,book3)
-print(shelf)
+# book= Book("King Solomon Mines")
+# book1= Book("Golang Microservices 101")
+# book3= Book("The god's of Egypt")
+# shelf = BookShelf(book,book1,book3)
+# print(shelf)
+
+class Book:
+    def __init__(self, name: str, page_count: int) -> None:
+        self.name= name
+        self.page_count=page_count
+        self.pages_read=0        
+    def __repr__(self) -> str:
+        return f"Book {self.name}, read{self.pages_read} pages out of {self.page_count} "
+    
+    def read(self,pages: int):
+        if self.pages_read + pages > self.page_count:
+            raise ValueError(
+                f"You tried reading {self.pages_read} + pages, but this book only has {self.page_count} pages"
+            )
+        self.pages_read +=pages
+        print(f"You have now read {self.pages_read} pages out of [self.page_count] ")
